a43bf003e562278b7fa410f3917f1004ac2bb25a
karthikch33 <saipavan39dh@gmail.com>
2025-10-24 12:34:49 +0530

    Updated Cards Data

--- LLM_Frontend/src/components/pages/CardsData.js
+++ LLM_Frontend/src/components/pages/CardsData.js
@@ -5,15 +5,9 @@ export const cardsData = [
         icon: 'fas fa-diagram-project',
         link:'/project/manageprojects' 
     },
-    // { 
-    //   title: 'Connections',
-    //   description: 'Manage your connections',
-    //   icon: 'fas fa-link',
-    //   link:'/connections/view-connections'
-    // },
     {
-      title: 'Tables',
-      description: 'Manage Your Tables',
+      title: 'Uploads',
+      description: 'Upload Your Files',
       icon: 'fas fa-upload', 
       link:'/connections/flatfile'
     },
@@ -22,17 +16,5 @@ export const cardsData = [
       description: 'Extract & Transform with ease.',
       icon: 'fas fa-brain', 
       link:'/workspace'
-    },
-    // {
-    //   title: 'PreLoad',
-    //   description: 'PreLoad',
-    //   icon: 'fas fa-brain', 
-    //   link:'/workspace/preload'
-    // },
-    // {
-    //   title: 'PostLoad',
-    //   description: 'PostLoad',
-    //   icon: 'fas fa-brain', 
-    //   link:'/workspace/postload'
-    // }
-  ];
+    }
+];
